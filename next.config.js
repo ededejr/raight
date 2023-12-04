368b05529211ebdaf5e6cf49f9b1acfe20a218cd
Edede Oiwoh <eoiwoh@gmail.com>
2023-12-04 14:04:18 -0500

    strip docker stuff and update manifest

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "standalone",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatar.edede.ca",
-        port: "",
-        pathname: "/",
-      },
-    ],
-  },
-};
-
+const nextConfig = {};
 module.exports = nextConfig;
